e7009dac01b9f4f1628f2945a5035071110ba160
roug <roug@gmail.com>
2006-07-25 11:55:21 +0000

    Simplify the linkkml file
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP/trunk/EEA-DAMS/website@39 906c100d-7b07-0410-bda9-df73dee6a547

--- linkkml.php
+++ linkkml.php
@@ -1,61 +1,49 @@
 <?php
-header('Content-Disposition: attachment; filename="link.kml"');
+header('Content-Disposition: attachment; filename="damposlink.kml"');
 header('Content-type: application/vnd.google-earth.kml+xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 ?>
 <kml xmlns="http://earth.google.com/kml/2.0">
-	<Document>
-		<name>European Dams</name>
-		<Snippet>Dams recorded in the EEA database.</Snippet>
-		<description><![CDATA[<p>Shows the 20 largest in view at any time.</p>
-		]]></description>
-		<visibility>1</visibility>
-		<open>1</open>
-		<LookAt>
-			<longitude>16</longitude>
-			<latitude>47</latitude>
-			<range>5000000.0</range>
-			<tilt>0.0</tilt>
-			<heading>0.0</heading>
-		</LookAt>
-		<NetworkLink>
-			<name>Validated dams</name>
-			<Snippet>Dams validated in Dampos</Snippet>
-			<description><![CDATA[<p>Reading from DAMPOS service</p>
-				]]></description>
-			<visibility>1</visibility>
-			<open>1</open>				
-			<Url>
-				<href>http://dampos.eea.europa.eu/showkml.php?coordinates=val</href>
-				<viewRefreshMode>onStop</viewRefreshMode>
-				<viewRefreshTime>1</viewRefreshTime>
-			</Url>
-		</NetworkLink>
-		<NetworkLink>
-			<name>Proposed dams</name>
-			<Snippet>Proposed dams</Snippet>
-			<description><![CDATA[<p>Reading from DAMPOS service</p>
-				]]></description>
-			<visibility>1</visibility>
-			<open>1</open>				
-			<Url>
-				<href>http://dampos.eea.europa.eu/showkml.php?coordinates=prop</href>
-				<viewRefreshMode>onStop</viewRefreshMode>
-				<viewRefreshTime>1</viewRefreshTime>
-			</Url>
-		</NetworkLink>
-		<NetworkLink>
-			<name>iCold dams</name>
-			<Snippet>iCold dams</Snippet>
-			<description><![CDATA[<p>Reading from DAMPOS service</p>
-				]]></description>
-			<visibility>1</visibility>
-			<open>1</open>				
-			<Url>
-				<href>http://dampos.eea.europa.eu/showkml.php?coordinates=icold</href>
-				<viewRefreshMode>onStop</viewRefreshMode>
-				<viewRefreshTime>1</viewRefreshTime>
-			</Url>
-		</NetworkLink>
-	</Document>
+  <Document>
+    <name>European Dams</name>
+    <description><![CDATA[Dams recorded in the EEA database.
+Shows the 20 largest in view at any time.]]></description>
+    <visibility>1</visibility>
+    <open>1</open>
+    <LookAt>
+      <longitude>15.89188611937717</longitude>
+      <latitude>53.37819505548502</latitude>
+      <altitude>0</altitude>
+      <range>3656450</range>
+      <tilt>0</tilt>
+      <heading>0.0</heading>
+    </LookAt>
+    <NetworkLink>
+      <name>Validated dams</name>
+      <description><![CDATA[Reading from DAMPOS service]]></description>
+      <Url>
+        <href>http://dampos.eea.europa.eu/showkml.php?coordinates=val</href>
+        <viewRefreshMode>onStop</viewRefreshMode>
+        <viewRefreshTime>3</viewRefreshTime>
+      </Url>
+    </NetworkLink>
+    <NetworkLink>
+      <name>Proposed dams</name>
+      <description><![CDATA[Reading from DAMPOS service]]></description>
+      <Url>
+        <href>http://dampos.eea.europa.eu/showkml.php?coordinates=prop</href>
+        <viewRefreshMode>onStop</viewRefreshMode>
+        <viewRefreshTime>3</viewRefreshTime>
+      </Url>
+    </NetworkLink>
+    <NetworkLink>
+      <name>iCold dams</name>
+      <description><![CDATA[ICOLD: International commission on large dams<br>http://www.icold-cigb.org]]></description>
+      <Url>
+        <href>http://dampos.eea.europa.eu/showkml.php?coordinates=icold</href>
+        <viewRefreshMode>onStop</viewRefreshMode>
+        <viewRefreshTime>3</viewRefreshTime>
+      </Url>
+    </NetworkLink>
+  </Document>
 </kml>
