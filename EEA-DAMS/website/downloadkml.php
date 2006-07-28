16995476f85a1217f397a80251b1268b0ebe2d3e
roug <roug@gmail.com>
2006-07-28 09:35:02 +0000

    Finishing touches. downloadkml.php merged with showkml.php linkkml.php renamed to googleearthlink.php
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP/trunk@40 906c100d-7b07-0410-bda9-df73dee6a547

--- EEA-DAMS/website/downloadkml.php
+++ EEA-DAMS/website/downloadkml.php
@@ -48,16 +48,29 @@ $dbconn = pg_connect("host=localhost dbname=EEADAMS user=postgres")
    or die('Could not connect: ' . pg_last_error());
 
 $country = '';
-$coordinates = 'val';
 if (isset($_GET['country'])) $country = strtoupper($_GET['country']);
+
+$coordinates = 'val';
 if (isset($_GET['coordinates'])) $coordinates = $_GET['coordinates'];
 
+$bbox="-180.0,-90.0,180.0,90.0";
+$limit = "ALL";
+if (isset($_GET['BBOX'])) {
+    $bbox = $_GET['BBOX'];
+    $limit = "20";
+}
+list($maxw,$maxs,$maxe,$maxn) = split(",",$bbox);
+
+header('Content-Disposition: attachment; filename="dam'.strtolower($country).'-'.$coordinates.'.kml"');
+header('Content-type: application/vnd.google-earth.kml+xml');
+
 // Performing SQL query
-$query = "SELECT dams.name, dams.x_$coordinates, dams.y_$coordinates, dams.ic_city, dams.river_id, dams.river_name, country.name AS cname, dams.ic_owner AS owner, dams.ic_engineer AS engineer, dams.ic_contractor AS contractor FROM dams INNER JOIN country ON dams.country=country.code WHERE dams.country LIKE '%$country%' AND x_$coordinates<>-32768 AND (x_$coordinates BETWEEN -30 AND 57) AND (y_$coordinates BETWEEN 18 AND 80) ORDER by noeea";
+$query = "SELECT dams.name, dams.x_$coordinates, dams.y_$coordinates, dams.ic_city, dams.river_id, dams.river_name, country.name AS cname, ic_owner, ic_engineer, dams.ic_contractor AS contractor, ic_capacity FROM dams INNER JOIN country ON dams.country=country.code WHERE dams.country LIKE '%$country%' AND (x_$coordinates BETWEEN $maxw AND $maxe) AND (y_$coordinates BETWEEN $maxs AND $maxn) ORDER by ic_capacity DESC LIMIT $limit";
 
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 if (!pg_num_rows($result)) {
-	echo '<p class="warning">No data available</p>';
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+        echo '<kml xmlns="http://earth.google.com/kml/2.0"></kml>';
 	// Free resultset
 pg_free_result($result);
 // Closing connection
@@ -65,9 +78,8 @@ pg_close($dbconn);
 
 }
 else {
-$file = 'dam';
-header('Content-Disposition: attachment; filename="'.$file.strtolower($country).'-'.$coordinates.'.kml"');
-header('Content-type: application/vnd.google-earth.kml+xml');
+
+
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 echo "<kml xmlns=\"http://earth.google.com/kml/2.0\">\n";
 echo "<Document>\n";
@@ -85,40 +97,47 @@ if ($country == '') {
 <Style id="icold">
   <IconStyle>
     <Icon>
-      <href>root://icons/palette-4.png</href>
-      <x>32</x>
-      <y>128</y>
-      <w>32</w>
-      <h>32</h>
+      <href>http://dampos.eea.europa.eu/img/icon/007red.png</href>
+      <w>37</w>
+      <h>37</h>
     </Icon>
   </IconStyle>
   <LabelStyle>
+      <color>ff8080ff</color>
   </LabelStyle>
+  <BalloonStyle>
+      <color>ff8080ff</color>
+  </BalloonStyle>
 </Style>
 <Style id="prop">
   <IconStyle>
     <Icon>
-      <href>root://icons/palette-3.png</href>
-      <x>128</x>
-      <w>32</w>
-      <h>32</h>
+      <href>http://dampos.eea.europa.eu/img/icon/007blue.png</href>
+      <w>37</w>
+      <h>37</h>
     </Icon>
   </IconStyle>
   <LabelStyle>
+      <color>ffffa0a0</color>
   </LabelStyle>
+  <BalloonStyle>
+      <color>ffffa0a0</color>
+  </BalloonStyle>
 </Style>
 <Style id="val">
   <IconStyle>
     <Icon>
-      <href>root://icons/palette-3.png</href>
-      <x>128</x>
-      <y>32</y>
-      <w>32</w>
-      <h>32</h>
+      <href>http://dampos.eea.europa.eu/img/icon/007green.png</href>
+      <w>37</w>
+      <h>37</h>
     </Icon>
   </IconStyle>
   <LabelStyle>
+      <color>ff80ff80</color>
   </LabelStyle>
+  <BalloonStyle>
+      <color>ff80ff80</color>
+  </BalloonStyle>
 </Style>
 <?php
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
@@ -132,9 +151,11 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo htmlspecialchars("Country: ".$line['cname']."<br>\n");
     echo htmlspecialchars("City: ".$line['ic_city']."<br>\n");
     echo htmlspecialchars("Hydrographic code: ".$line['river_id']."<br>\n");
-    echo htmlspecialchars("Owner: ".$line['owner']."<br>\n");
-    echo htmlspecialchars("Engineer: ".$line['engineer']."<br>\n");
+    echo htmlspecialchars("Owner: ".$line['ic_owner']."<br>\n");
+    echo htmlspecialchars("Engineer: ".$line['ic_engineer']."<br>\n");
     echo htmlspecialchars("Contractor: ".$line['contractor']."<br>\n");
+    echo htmlspecialchars("Capacity: ".$line['ic_capacity']."<br>\n");
+    echo htmlspecialchars("Coordinates: ".$line["x_$coordinates"].','.$line["y_$coordinates"]."<br>\n");
     echo "</description>\n";
     echo "<open>0</open>\n";
     echo "<styleUrl>#$coordinates</styleUrl>\n";
