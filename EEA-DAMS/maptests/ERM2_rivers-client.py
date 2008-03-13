5617937e37c14ccf87c1378aebd711e48d89940b
roug <roug@gmail.com>
2008-03-13 10:55:11 +0000

    Updated to test some other areas
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP/trunk@75 906c100d-7b07-0410-bda9-df73dee6a547

--- EEA-DAMS/maptests/ERM2_rivers-client.py
+++ EEA-DAMS/maptests/ERM2_rivers-client.py
@@ -18,15 +18,20 @@ def getamap(url,layers):
     'BGCOLOR':'0xFFFFFF',
     'TRANSPARENT':'TRUE',
     'SRS':'EPSG:4326',
-    'BBOX':'13.9,50.9,14.0,51.0',
-    'WIDTH':'256',
-    'HEIGHT':'256',
+#   'BBOX':'13.85,50.9,13.95,51.0', # Elbe & Kiessee
+#   'BBOX':'9.2,47.5,9.5,47.8', # Bodensee/Lake Constance
+    'BBOX':'11.2,53.5,11.5,53.8', # Schweriner See
+    'WIDTH':'512',
+    'HEIGHT':'512',
     'reaspect':'false'
     }
     f = urllib.urlopen(makeurl(url,params))
     d = f.read()
-#   print f.info()
+    m = f.info()
     f.close()
+    if m.gettype() in ("application/vnd.ogc.se_xml","text/html"):
+        print d
+        return
     mapf = file("testmap.png","wb")
     mapf.write(d)
     mapf.close()
@@ -36,4 +41,4 @@ from wmsclient import getcapabilities
 if __name__ == '__main__':
     url="http://dampos-demo.eea.europa.eu/cgi-bin/wseea?"
 #   getcapabilities(url)
-    getamap(url,'ERM2_rivers')
+    getamap(url,'ERM2Water')
