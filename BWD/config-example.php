3b806da8648d1fd81a5a434265ec94ccd98151d2
roug <roug@gmail.com>
2008-06-04 11:01:04 +0000

    Added an example of the configuration
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP/trunk@104 906c100d-7b07-0410-bda9-df73dee6a547

---
+++ BWD/config-example.php
@@ -0,0 +1,14 @@
+<?php
+
+// ************************************
+// *** NASTAVITEV SPR. ZA POIZVEDBO ***
+// ************************************
+
+$database = "bwd";
+
+$host = "localhost";
+$dbuser = "databaseuser";
+$dbpass = "password";
+
+
+?>
