e713b4162481ae6b1882ce093179d61375f97ffa
roug <roug@gmail.com>
2008-06-04 10:38:42 +0000

    Forgot the namespace declaration
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP/trunk@103 906c100d-7b07-0410-bda9-df73dee6a547

--- BWD/website/index.php
+++ BWD/website/index.php
@@ -21,7 +21,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <title>Bathing water quality data/map viewer</title>
   <link href="template.css" rel="stylesheet" type="text/css" />
