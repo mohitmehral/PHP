61c7455c2de8a9893501e3782576fe7e8d77fbd0
roug <roug@gmail.com>
2009-03-12 11:41:33 +0000

    Fix EUNIS references
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP/branches/PAMDB-with-EEA-style@140 906c100d-7b07-0410-bda9-df73dee6a547

--- htdocs/accessibility.php
+++ htdocs/accessibility.php
@@ -4,11 +4,11 @@ include('conx/db_conx_open.php');
 require_once 'support.php';
 standard_html_header("")
 ?>
-                <h1>
-                Accessibility statement
-                </h1>
-                <br />
-                <h2>Keyboard shortcuts</h2>
+<h1>
+Accessibility statement
+</h1>
+
+<h2>Keyboard shortcuts</h2>
 <p>Most browsers support jumping to specific links by typing 
 keys defined on the web site. On Windows, you can press
 <strong>ALT + an access key</strong>; on Macintosh, you can 
@@ -25,56 +25,26 @@ access key 9 - feedback and helpdesk
 access key 0 - link to accessibility statement (this page) 
 </li>
 <li>
-access key l - link to login 
-</li>
-<li>
-access key r - link to references
-</li>
-<li>
-access key p - link to reports
-</li>
-<li>
-access key s - link to Species module
-</li>
-<li>
-access key h - link to Habitat types module
-</li>
-<li>
-access key t - link to Sites module
-</li>
-<li>
 access key 3 - link to site map
 </li>
 <li>
 access key g - link to glossary
 </li>
 <li>
-access key u - link to GIS Tool
-</li>
-<li>
-access key b - link to introduction on EUNIS Database
+access key b - link to introduction
 </li>
 </ul>
 <h2>Standards compliance</h2>
 <ul>
 <li>the
-<a title="Web Content Accessibility Guidelines" href="http://www.w3.org/TR/WAI-WEBCONTENT/wai-pageauth.html">
+<a href="http://www.w3.org/TR/WAI-WEBCONTENT/wai-pageauth.html">
 Web Content Accessibility Guidelines 1.0</a> (WAI-AA) 
 </li>
 <li>pages validate
-<a title="XHML specifications" href="http://www.w3.org/TR/xhtml1/">XHTML 1.0 
-Transitional</a></li>
-<li>pages validate HTML 4.01 </li>
+<a href="http://www.w3.org/TR/xhtml1/">XHTML 1.0 Transitional</a></li>
 <li>the Cascading Style Sheets implemented respect the
-<a title="CSS2 specifications" href="http://www.w3.org/TR/REC-CSS2/">CSS2 
-Specification</a> </li>
+<a href="http://www.w3.org/TR/REC-CSS2/">CSS2 Specification</a> </li>
 <li>the web site is still usable without style sheets</li>
-<li>there are still some special pages, like the GIS Tool - implemented in 
-Macromedia Flash and some pages where navigation is based heavily on JavaScript 
-and specific HTML elements where equivalent accessible page content can't be 
-provided and the browsers which don't implement JavaScript will experience 
-reduced functionality. Special measures where taken to announce the user when 
-some functionality is available only on some browsers.</li>
 </ul>
 <h2>Navigation aids</h2>
 <ul>
@@ -82,33 +52,27 @@ some functionality is available only on some browsers.</li>
 from the current location to the root 
 </li>
 <li>the navigation for the regular end-users does not 
-imply opening of pop-ups windows (note that some of the 
-administrative pages with more complex functionality do) 
+imply opening of pop-ups windows 
 and the return to the previous page can be easily done 
 through the <em>back</em> button from the browser</li>
-<li>an accessibility link to the main content for every page is provided</li>
+<li>an accessibility link is provided to the main content for every page</li>
 </ul>
 <h2>Links</h2>
 <ul>
 <li>links have <em>TITLE</em> attributes which describe the link</li>
-<li>links' names make sense out of context 
+<li>link names make sense out of context</li>
+<li>the links' colour is chosen to contrast sufficiently 
+with the background colour, and they are distinguishable 
+by having a different text-decoration.
 </li>
-<li>the links' color is chosen to contrast sufficiently 
-with the background color, and they are distinguishable 
-by having a different text-decoration. Users can choose a personalized theme for 
-the web site.  
-</li>
-<li>visited links and active ones are marked with 
-different colors </li>
+<li>visited links and active ones are marked with different colours </li>
 </ul>
 <h2>Images</h2>
 <ul>
-<li>all content images used in the website include 
-descriptive <em>ALT</em> attributes 
+<li>all content images used in the website include descriptive <em>ALT</em> attributes 
 </li>
 <li>purely decorative graphics include null <em>ALT</em> 
-attributes so they will not be interpreted by speech 
-synthesizers </li>
+attributes so they will not be interpreted by speech synthesizers </li>
 </ul>
 <h2>Visual design</h2>
 <ul>
@@ -117,8 +81,8 @@ synthesizers </li>
 <li>only relative font sizes are used, compatible with 
 the user-specified "text size" option in visual browsers 
 </li>
-<li>the website uses color safely. The pages layout is 
-designed in web safe colors 
+<li>the website uses colour safely. The pages layout is 
+designed in web safe colours 
 </li>
 <li>if your browser or browsing device used does not 
 support stylesheets at all, the content of each page is 
@@ -131,7 +95,7 @@ appropriate DOCTYPE declaration</li>
 <li>The language is identified on every page of the web 
 site</li>
 <li>The heading(s) element of every page - h5 and h6 - have a title 
-specific to he page content:
+specific to the page content:
 </li>
 <li>Semantically correct markup: the content is well 
 structured and uses the correct HTML tags for the 
